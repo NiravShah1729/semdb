14ae7c72fcb4f479b1b5ece081d8c94a874915d3
NiravShah1729 <nirav.s@ahduni.edu.in>
2026-08-01 00:21:01 +0530

    feat: implement semantic storage and command support with vector embedding integration

--- store/semantic.go
+++ store/semantic.go
@@ -30,3 +30,42 @@ func cosineSimilarity(a, b []float32) float32 {
     }
     return dot / (float32(math.Sqrt(float64(normA))) * float32(math.Sqrt(float64(normB))))
 }
+
+func (s *SemanticStore) Add(key, text,value string , vector []float32){
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	s.entries = append(s.entries, SemanticEntry{
+		Key: key,
+		Text: text,
+		Vector: vector,
+		Value: value,
+	})
+}
+
+func (s *SemanticStore) Search(vector []float32,threshold float32) (string, bool){
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	if len(s.entries) == 0{
+		return "",false
+	}
+
+	var maxi float32
+	maxi = 0
+
+	var temp SemanticEntry
+	for _,val := range s.entries {
+		similarity := cosineSimilarity(vector,val.Vector)
+		if similarity > maxi {
+			maxi = similarity
+			temp = val
+		}
+	}
+
+	if maxi >= threshold {
+		return temp.Value,true
+	}else{
+		return "",false
+	}
+}
